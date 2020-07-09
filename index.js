3aa7a2c257ff0215b6161f920c4b60c6bbaa38fd
Shoichi Hasegawa <shoichihasegawa2018@hotmail.com>
2020-07-10 02:30:06 +0900

    added post request

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ app.use(express.json());
 
 const fruits = [
   {name: 'Apples', id: 1},
-  {name: 'Banana', id: 1},
-  {name: 'Grape', id: 1},
-  {name: 'Orange', id: 1}
+  {name: 'Banana', id: 2},
+  {name: 'Grape', id: 3},
+  {name: 'Orange', id: 4}
 ]
 
 app.get('/', (req,res) => {
@@ -23,6 +23,29 @@ app.get('/api/fruits/:id', (req,res) => {
   if(!fruit)res.status(404).send('fruit not found');
   res.send(fruit);
 });
+
+
+
+app.post('api/fruits', (req, res) => {
+  const { error } = validateFruit(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message)
+    return;
+  }
+  const fruit = {
+    id: fruits.length + 1,
+    name: req.body.name
+  };
+  fruits.push(fruit);
+  res.send(fruit);
+});
+
+function validateFruit(fruit) {
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+  return Joi.validate(fruit, schema);
+}
 
 const port = process.env.PORT || 5001;
   app.listen(port, () => console.log(`listening on port ${port}...`));
