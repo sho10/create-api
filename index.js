97ae7bb21507b6503093c822e5ec89a995f4e245
Shoichi Hasegawa <shoichihasegawa2018@hotmail.com>
2020-07-10 16:13:50 +0900

    added validation for post

--- index.js
+++ index.js
@@ -26,26 +26,32 @@ app.get('/api/fruits/:id', (req,res) => {
 
 
 
-app.post('api/fruits', (req, res) => {
-  const { error } = validateFruit(req.body);
-  if (error) {
-    res.status(400).send(error.details[0].message)
-    return;
-  }
-  const fruit = {
-    id: fruits.length + 1,
-    name: req.body.name
-  };
-  fruits.push(fruit);
-  res.send(fruit);
+app.post('/api/fruits', (req, res) => {
+const { error } = validateFruit(req.body);
+if (error) {
+res.status(400).send(error.details[0].message)
+return;
+}
+const fruit = {
+id: fruits.length + 1,
+name: req.body.name
+};
+fruits.push(fruit);
+res.send(fruit);
 });
 
 function validateFruit(fruit) {
-  const schema = {
-    name: Joi.string().min(3).required()
-  };
-  return Joi.validate(fruit, schema);
+  const schema = Joi.object({ name: Joi.string() .min(3) .required() });
+  const validation = schema.validate(fruit);
+  return validation;
 }
+
+// function validateFruit(fruit) {
+//   const schema = {
+//     name: Joi.string().min(3).required()
+//   };
+//   return Joi.validate(fruit, schema);
+// }
 
 const port = process.env.PORT || 5001;
   app.listen(port, () => console.log(`listening on port ${port}...`));
